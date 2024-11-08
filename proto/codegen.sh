1b2fbae133f403acfd0b8eafb54ea4a12a8ad911
totoyk <totoyk104@gmail.com>
2024-11-09 00:51:57 +0900

    update refactor proto/codegen.sh

--- proto/codegen.sh
+++ proto/codegen.sh
@@ -7,6 +7,7 @@ DESCRIPTOR_DIR=./proxy/pb
 
 ####### Generate Go and JS code #######
 for proto in $(find $PROTO_DIR -name '*.proto' | sed 's!^.*/!!'); do
+  echo $proto is processing...
   PROTO_NAME=$(echo $proto | sed 's!\.proto!!')
   mkdir -p $GO_DIR/$PROTO_NAME
   mkdir -p $JS_DIR/$PROTO_NAME
@@ -15,9 +16,4 @@ for proto in $(find $PROTO_DIR -name '*.proto' | sed 's!^.*/!!'); do
   /usr/local/bin/entrypoint.sh -f $PROTO_DIR/$proto -l go -o $GO_DIR/$PROTO_NAME
   /usr/local/bin/entrypoint.sh -f $PROTO_DIR/$proto -l descriptor_set -o $DESCRIPTOR_DIR/$PROTO_NAME
   /usr/local/bin/entrypoint.sh -f $PROTO_DIR/$proto -l web -o $JS_DIR/$PROTO_NAME --js_out=import_style=commonjs --grpc-web_out=import_style=commonjs+dts
-  # protoc \
-  # --js_out=import_style=commonjs:$JS_DIR/$PROTO_NAME \
-  # --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:$JS_DIR/$PROTO_NAME \
-
-  #   $PROTO_DIR/$proto
 done
