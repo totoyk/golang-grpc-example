90c950a72e48291b36a9f37f9c1377a3b0b69ab0
totoyk <totoyk104@gmail.com>
2024-11-24 17:14:34 +0900

    refactor client protofile and directory

--- proto/codegen.sh
+++ proto/codegen.sh
@@ -2,7 +2,7 @@
 ####### Variables #######
 PROTO_DIR=./proto
 GO_DIR=./api/internal/pb
-JS_DIR=./client/src
+JS_DIR=./client/src/grpc_web
 DESCRIPTOR_DIR=./proxy/pb
 
 ####### Generate Go and JS code #######
@@ -10,10 +10,10 @@ for proto in $(find $PROTO_DIR -name '*.proto' | sed 's!^.*/!!'); do
   echo $proto is processing...
   PROTO_NAME=$(echo $proto | sed 's!\.proto!!')
   mkdir -p $GO_DIR
-  mkdir -p $JS_DIR/$PROTO_NAME
+  mkdir -p $JS_DIR
   mkdir -p $DESCRIPTOR_DIR
 
   /usr/local/bin/entrypoint.sh -f $PROTO_DIR/$proto -l go -o $GO_DIR
   /usr/local/bin/entrypoint.sh -f $PROTO_DIR/$proto -l descriptor_set -o $DESCRIPTOR_DIR/$PROTO_NAME
-  /usr/local/bin/entrypoint.sh -f $PROTO_DIR/$proto -l web -o $JS_DIR/$PROTO_NAME --js_out=import_style=commonjs --grpc-web_out=import_style=commonjs+dts
+  /usr/local/bin/entrypoint.sh -f $PROTO_DIR/$proto -l web -o $JS_DIR --js-out "import_style=commonjs" --grpc-web-out "import_style=commonjs+dts"
 done
