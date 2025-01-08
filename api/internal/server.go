59958b72fada39f2f35f1c602d65460d47cb0158
totoyk <totoyk104@gmail.com>
2025-01-08 17:31:14 +0900

    hotfix logger err handling

--- api/internal/server.go
+++ api/internal/server.go
@@ -25,6 +25,10 @@ func Run() int {
 
 	// TODO: enable log level from env
 	logger, err := logging.NewLogger("info")
+	if err != nil {
+		log.Fatalf("failed to create logger: %v", err)
+		return 1
+	}
 
 	grpcServer := grpc.NewServer(
 		grpc_middleware.WithUnaryServerChain(
